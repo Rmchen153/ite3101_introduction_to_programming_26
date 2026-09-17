3fb50838cf8e286ef246fdfbdfe1d876c6269090
CHEN Wai Ming <chenweiming810725@gmail.com>
2026-09-17 08:14:35 +0000

    [skip actions] lab/lab01/ch01_t10_two_types_of_division.py

--- lab/lab01/ch01_t10_two_types_of_division.py
+++ lab/lab01/ch01_t10_two_types_of_division.py
@@ -4,4 +4,5 @@ whole_cucumbers_per_person = cucumbers//num_peopleprint(
     whole_cucumbers_per_person)
 
 float_cucumbers_per_person = cucumbers
-print(num_peopleprint(float_cucumbers_per-person)
+print(num_people
+      print(float_cucumbers_per-person)
