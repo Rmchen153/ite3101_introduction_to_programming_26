c0660fd09e0585c8e778534bf007ca8f6bba8eeb
CHEN Wai Ming <chenweiming810725@gmail.com>
2026-09-24 08:48:25 +0000

    [skip actions] lab/lab04/ch04_t06_and.py

--- lab/lab04/ch04_t04_how_tables_have_turned.py
+++ lab/lab04/ch04_t04_how_tables_have_turned.py
@@ -4,13 +4,13 @@
 bool_one = 3 < 5  # We already did this one for you!
 
 # Make me false!
-bool_two = 3>5
+bool_two = 3 > 5
 
 # Make me true!
-bool_three = 3!=5
+bool_three = 3 != 5
 
 # Make me false!
-bool_four = 3==5
+bool_four = 3 == 5
 
 # Make me true!
-bool_five = 3<=5
+bool_five = 3 <= 5
