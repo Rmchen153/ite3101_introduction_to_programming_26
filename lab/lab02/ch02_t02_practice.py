1a2ec066b0ae618825c7a41058ce452320782a45
CHEN Wai Ming <chenweiming810725@gmail.com>
2026-09-24 07:18:43 +0000

    [skip actions] lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,8 +1,8 @@
 # Assign your variables below, each on its own line!
 
-caesar to "Graham"
--	Set praline to "John"
--	Set viking to "Teresa
+caesar="Graham"
+praline to "John"
+viking to "Teresa
 
 
 # Put your variables above this line, and uncomment the following 3 line of codes.
